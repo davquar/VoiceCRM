61ed08d87c117d0ab41775edaff72f3d03487df4
Davide Quaranta <30431538+fortym2@users.noreply.github.com>
2021-07-10 09:55:28 +0200

    Update docker-compose, and rename skill

--- crm-skill/__init__.py
+++ crm-skill/__init__.py
@@ -1,19 +1,17 @@
 from mycroft import MycroftSkill, intent_file_handler
 
 
-class Sample(MycroftSkill):
+class VoiceCRM(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('sample.intent')
-    def handle_sample(self, message):
-        contact = message.data.get('contact')
-
-        self.speak_dialog('sample', data={
-            'contact': contact
-        })
+    @intent_file_handler('new-contact.intent')
+    def handle_new_contact(self, message):
+        self.speak_dialog("ask-surname", expect_response=True)
+        resp = self.get_response("ask-surname")
+        print("RESPONSE: " + resp)
 
 
 def create_skill():
-    return Sample()
+    return VoiceCRM()
 
